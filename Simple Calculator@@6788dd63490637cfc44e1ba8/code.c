04d3f669773c6cb51dcf4f8ba6507975f84c9cab
kashish012-c <chaudharykashish514@gmail.com>
2025-02-16 13:52:58 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,45 +1,25 @@
 #include <stdio.h>
 int main() {
-    //  int a;
-    //  int b;
-    //  char ch;
-    //  scanf("%d %d",&a,&b);
-    //  scanf(" %c",&ch);
-    //  switch(ch){
-    //      case '+': printf("%d",a+b);
-    //                break;
-    //      case '-': printf("%d",a-b);
-    //                break;
-    //      case '*': printf("%d",a*b);
-    //                break;
-    //      case '/':
-    //      if(b==0){
-    //          printf("error");
-    //      }
-    //      else{
-    //          printf("%d",a/b);
-    //          break;
-    //      }
-    //  } 
-    int a,b;
-    char ch;
-    scanf("%d %d",&a,&b);
-    scanf("%c",&ch);
-    switch(ch){
-        case '+': printf("%d",a+b);
-                  break;
-        case '-': printf("%d",a-b);
-                  break;
-        case '*': printf("%d",a*b);
-                  break;
-        case '/':
-        if(b==0){
-            printf("error");
-        }
-        else{
-            printf("%d",a/b);
-            break;
-        }
-    }
+      int a;
+      int b;
+      char ch;
+      scanf("%d %d",&a,&b);
+      scanf(" %c",&ch);
+      switch(ch){
+          case '+': printf("%d",a+b);
+                    break;
+          case '-': printf("%d",a-b);
+                    break;
+          case '*': printf("%d",a*b);
+                    break;
+          case '/':
+          if(b==0){
+              printf("error");
+          }
+          else{
+              printf("%d",a/b);
+              break;
+          }
+      } 
     return 0;
 }
